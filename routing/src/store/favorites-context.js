116c05fec654ac1a025e462294442028eadad36b
Jacob Mompean <jacobmompean@Jacobs-MacBook-Air.local>
2022-09-11 23:38:02 -0500

    Add context

---
+++ routing/src/store/favorites-context.js
@@ -0,0 +1,27 @@
+import { createContext, useState } from 'react';
+
+const FavoritesContext = createContext({
+  favorites: [],
+  totalFavorites: 0,
+});
+
+function FavoritesContextProvider(props) {
+  const [userFavories, setUserFavorites] = useState();
+
+  function addFavoriteHandler() {}
+
+  function removeFavoriteHandler() {}
+
+  function itemIsFavoriteHandler() {}
+
+  const context = {
+    favorites: userFavories,
+    totalFavorites: userFavories.length,
+  };
+
+  return (
+    <FavoritesContext.Provider value={context}>
+      {props.children}
+    </FavoritesContext.Provider>
+  );
+}
